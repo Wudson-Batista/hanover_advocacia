bd3c3b1c6cee21532563baea3159bc2743b5319b
Wudson Batista <wudson06b.roberto@gmail.com>
2023-06-28 20:31:21 -0300

    Actuations page

---
+++ src/Pages/Actuations.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../Components/Banner'
+
+function Actuations() {
+    return (
+        <div>
+            <Banner></Banner>
+        </div>
+    )
+}
+
+export default Actuations
