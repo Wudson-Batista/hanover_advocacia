60433e43e463cc0fff2d29a2c71e4a36ce6735af
Wudson Batista <wudson06b.roberto@gmail.com>
2023-06-29 00:54:19 -0300

    Using the new title and services section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import Banner from '../Components/Banner'
 import banner from '../assets/banner-bg.png'
 import Button from '../Components/Button'
+import Title from '../Components/Title'
+import ServicesSection from '../Components/sections/ServicesSection'
 
 function Home() {
     return (
-        <div className=''>
+        <main className=''>
             <Banner bg={banner}>
                 <div className="container flex h-[calc(70vh)] items-center">
-                    <div className='md:max-w-2xl uppercase'>
-                        <h3 className="subtitle font-medium font-serif md:text-md sm:text-md text-sm text-gold-400 mb-1">Hanover Advocacia: Seu Parceiro Jurídico de Confiança</h3>
-                        <h1 className="title font-semibold md:text-3xl text-xl">Soluções jurídicas especializadas para empresas e indivíduos</h1>
+                    <div className='md:max-w-2xl '>
+                        <Title
+                            subtitle={'Hanover Advocacia: Seu Parceiro Jurídico de Confiança'}
+                            title={'Soluções jurídicas especializadas para empresas e indivíduos'}
+                        />
                         <Button bg='border-gold-400 border-2 text-gold-400 hover:bg-gold-400 hover:text-white mt-3'>Entre em contato</Button>
                     </div>
                 </div>
             </Banner>
-        </div>
+            <ServicesSection></ServicesSection>
+        </main>
     )
 }
 
