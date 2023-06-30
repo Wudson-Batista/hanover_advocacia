e237552690b777a343cd6c1db079ece21f29c251
Wudson Batista <wudson06b.roberto@gmail.com>
2023-06-30 00:07:57 -0300

    Contact section

---
+++ src/Components/sections/ContactSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Form from '../Form'
+import Title from '../Title'
+
+function ContactSection() {
+    return (
+        <section className='my-16 bg-about'>
+            <div className='container grid grid-cols-2 gap-5'>
+                <Form></Form>
+                
+            </div>
+
+        </section>
+    )
+}
+
+export default ContactSection
