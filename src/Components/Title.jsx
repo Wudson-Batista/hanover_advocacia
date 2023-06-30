487497a5844e453e79d438a8ec44b0d0f2f6a486
Wudson Batista <wudson06b.roberto@gmail.com>
2023-06-30 00:08:08 -0300

    Inverted title

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
-function Title({ title, subtitle }) {
-
+function Title({ title, subtitle, inverted }) {
     return (
-        <div className='uppercase'>
-            <h3 className={`md:text-md sm:text-md text-sm subtitle font-medium font-serif text-gold-400 mb-1`}>
-                {subtitle}
-            </h3>
-            <h1 className={`md:text-3xl text-xl title font-semibold`}>{title}</h1>
-        </div >
+        !inverted ? (
+            <div className='uppercase'>
+                <h3 className={`md:text-md sm:text-md text-sm subtitle font-medium font-serif text-gold-400 mb-1`}>
+                    {subtitle}
+                </h3>
+                <h1 className={`md:text-3xl text-xl title font-semibold`}>{title}</h1>
+            </div >
+        )
+            : (
+                <div className=''>
+                    <h3 className={`md:text-3xl uppercase text-xl subtitle font-medium font-serif text-gold-400 mb-1`}>
+                        {subtitle}
+                    </h3>
+                    <h1 className={`text-white md:text-md sm:text-md text-sm title font-semibold`}>{title}</h1>
+                </div >
+            )
     )
 }
 
