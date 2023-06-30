21c3aa6f67d1ffc5004d534b1e8975322a89bba2
Wudson Batista <wudson06b.roberto@gmail.com>
2023-06-30 00:07:45 -0300

    Form component

---
+++ src/Components/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import FormInput from './FormInput'
+import Title from './Title'
+
+function Form() {
+    return (
+        <form action="" className='py-8'>
+            <Title inverted={true} subtitle='Vamos conversar?' title='Entre em contato com o nosso escritório e descubra como nossos advogados podem te ajudar'></Title>
+            <div className='mt-8 flex flex-col gap-4'>
+                <FormInput label='Nome' isRequired={true} inputType='text'></FormInput>
+                <FormInput label='E-mail' isRequired={true} inputType='email'></FormInput>
+                <FormInput label='Telefone' inputType='text'></FormInput>
+                <FormInput label='Mensagem' isRequired={true} inputType='textarea'></FormInput>
+            </div>
+        </form>
+    )
+}
+
+export default Form
