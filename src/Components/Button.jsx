c97db8f264dcc41bfa9f2660c0fe255493465345
Wudson Batista <wudson06b.roberto@gmail.com>
2023-06-30 22:59:50 -0300

    Some colors adjustments

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,9 +4,7 @@ function Button({ children, icon, variant }) {
 
     const outlineGoldButton = `rounded transition border-gold-400 border-2 text-gold-400 hover:bg-gold-400 hover:text-white mt-3 px-5 py-3 uppercase font-semibold tracking-wide`;
 
-    const solidGoldButton = `rounded transition bg-gold-400 hover:opacity-75 mt-3 px-5 py-3 uppercase font-semibold tracking-wide`;
-
-
+    const solidGoldButton = `rounded transition bg-gold-400 hover:opacity-70 mt-3 px-5 py-3 uppercase font-semibold tracking-wide`;
 
     return (
         icon ? (
--- src/Components/FormInput.jsx
+++ src/Components/FormInput.jsx
@@ -6,7 +6,7 @@ function FormInput({ label, inputType, isRequired }) {
         inputType === 'textarea' ? (
 
             <div className="relative">
-                <textarea type={inputType} id={`floating_${label}`} className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border border-blue-grey-300/80 appearance-none  focus:outline-none focus:ring-0 focus:border-gold-400 peer resize-none" placeholder=" " required={isRequired} ></textarea>
+                <textarea type={inputType} id={`floating_${label}`} className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-white bg-transparent rounded-lg border border-blue-grey-300 appearance-none  focus:outline-none focus:ring-0 focus:border-gold-400 peer resize-none" placeholder=" " required={isRequired} ></textarea>
 
                 <label htmlFor={`floating_${label}`} className="absolute text-md text-blue-grey-300 duration-300 transform -translate-y-6 scale-75 top-2 z-10 bg-about origin-[0] cursor-text px-2 peer-focus:px-2 peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/4 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-5 left-1">{label} {isRequired ? '*' : ''}</label>
             </div>
