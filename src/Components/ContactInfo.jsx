b715cd1389e76b70a45ec350ea0c27068e0a56d6
Wudson Batista <wudson06b.roberto@gmail.com>
2023-07-02 00:37:22 -0300

    Align fixed

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ContactInfo({ info, icon }) {
     return (
-        <div className='flex md:flex-row flex-col text-center lg:gap-4 gap-2 items-center'>
+        <div className='flex md:flex-row flex-col max-md:text-center lg:gap-4 gap-2 items-center'>
             <div className="shrink-0 phone border border-blue-grey-200/30 w-12 h-12 rounded-full flex justify-center items-center shadow">
                 {icon}
             </div>
