c22bca9ced93b524547bfa6380de69dd5881e0c5
Wudson Batista <wudson06b.roberto@gmail.com>
2023-07-03 21:24:44 -0300

    Cards obj export (temporary)

--- src/Components/sections/ServicesSection.jsx
+++ src/Components/sections/ServicesSection.jsx
@@ -14,57 +14,58 @@ import Button from '../Button'
 
 import { FaChevronRight } from 'react-icons/fa'
 
+const cards = [
+    {
+        id: 1,
+        icon: briefcase,
+        title: "Direito Empresarial",
+        description: "Somos um escritório de advocacia especialista em Direito Empresarial. Contamos com uma equipe multidisciplinar pronta para melhor defender nossos clientes.",
+    },
+    {
+        id: 2,
+        icon: worker,
+        title: "Direito Trabalhista",
+        description: "Relações familiares, por vezes, geram conflitos. Atuamos com perícia e cuidado nos assuntos advindos do Direito de Família.",
+    },
+    {
+        id: 3,
+        icon: family,
+        title: "Direito de Familia",
+        description: "Relações familiares, por vezes, geram conflitos. Atuamos com perícia e cuidado nos assuntos advindos do Direito de Família.",
+    },
+    {
+        id: 4,
+        icon: money,
+        title: "Direito Tributário",
+        description: "O advogado em Direito Tributário realiza serviços de natureza judicial e administrativa, promovendo a defesa nas mais diversas áreas da esfera tributária.",
+    },
+    {
+        id: 5,
+        icon: handcuffs,
+        title: "Direito Criminal",
+        description: "O Direito Criminal se relaciona a atos dentro da esfera penal. Contamos com equipe capacitada na área para melhor defender os direitos de nossos clientes.",
+    },
+    {
+        id: 6,
+        icon: shop,
+        title: "Direito do Consumidor",
+        description: "O Direito do Consumidor está relacionado aos conflitos advindos das relações de consumo. Contamos com uma equipe especialista no assunto.",
+    },
+    {
+        id: 7,
+        icon: hacker,
+        title: "Direito Cibernético",
+        description: "Novas tecnologias levam, também, a novos conflitos. Prestamos serviços jurídicos especializados em Direito Digital.",
+    },
+    {
+        id: 8,
+        icon: home,
+        title: "Direito Imobiliário",
+        description: "Somos um escritório de advocacia especialista em Direito Imobiliário. Contamos com equipe multidisciplinar pronta para melhor defender nossos clientes.",
+    },
+]
+
 function ServicesSection() {
-    const cards = [
-        {
-            id: 1,
-            icon: briefcase,
-            title: "Direito Empresarial",
-            description: "Somos um escritório de advocacia especialista em Direito Empresarial. Contamos com uma equipe multidisciplinar pronta para melhor defender nossos clientes.",
-        },
-        {
-            id: 2,
-            icon: worker,
-            title: "Direito Trabalhista",
-            description: "Relações familiares, por vezes, geram conflitos. Atuamos com perícia e cuidado nos assuntos advindos do Direito de Família.",
-        },
-        {
-            id: 3,
-            icon: family,
-            title: "Direito de Familia",
-            description: "Relações familiares, por vezes, geram conflitos. Atuamos com perícia e cuidado nos assuntos advindos do Direito de Família.",
-        },
-        {
-            id: 4,
-            icon: money,
-            title: "Direito Tributário",
-            description: "O advogado em Direito Tributário realiza serviços de natureza judicial e administrativa, promovendo a defesa nas mais diversas áreas da esfera tributária.",
-        },
-        {
-            id: 5,
-            icon: handcuffs,
-            title: "Direito Criminal",
-            description: "O Direito Criminal se relaciona a atos dentro da esfera penal. Contamos com equipe capacitada na área para melhor defender os direitos de nossos clientes.",
-        },
-        {
-            id: 6,
-            icon: shop,
-            title: "Direito do Consumidor",
-            description: "O Direito do Consumidor está relacionado aos conflitos advindos das relações de consumo. Contamos com uma equipe especialista no assunto.",
-        },
-        {
-            id: 7,
-            icon: hacker,
-            title: "Direito Cibernético",
-            description: "Novas tecnologias levam, também, a novos conflitos. Prestamos serviços jurídicos especializados em Direito Digital.",
-        },
-        {
-            id: 8,
-            icon: home,
-            title: "Direito Imobiliário",
-            description: "Somos um escritório de advocacia especialista em Direito Imobiliário. Contamos com equipe multidisciplinar pronta para melhor defender nossos clientes.",
-        },
-    ]
 
     return (
         <section className='my-24'>
@@ -73,16 +74,22 @@ function ServicesSection() {
                     subtitle='Nossos serviços'
                     title='Escritório de advocacia'
                 />
-                <div className="cards mt-12 grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2   gap-6">
-                    {cards.map(({ title, description, icon, id }) => (
-                        <ServiceCard
-                            title={title}
-                            description={description}
-                            icon={icon}
-                            id={id}
-                            key={id}
-                        />
-                    ))}
+                <div className="cards mt-12 grid lg:grid-cols-3 sm:grid-cols-2 gap-6">
+                    {
+                        cards.map(({ title, description, icon, id }, i) => (
+                            i < 6 && (
+                                <ServiceCard
+                                    title={title}
+                                    description={description}
+                                    icon={icon}
+                                    id={id}
+                                    key={id}
+                                />
+                            )
+                        ))
+
+
+                    }
                 </div>
                 <div className='flex md:justify-center mt-4'>
                     <Button variant='outline' icon={<FaChevronRight className='text-lg' />}>Ver mais atuações</Button>
@@ -92,4 +99,4 @@ function ServicesSection() {
     )
 }
 
-export default ServicesSection
+export { ServicesSection, cards }
