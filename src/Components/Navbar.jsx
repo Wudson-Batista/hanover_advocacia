8b2209157dbb51d3fd51739e0ffbc1821172a132
Wudson Batista <wudson06b.roberto@gmail.com>
2023-07-04 21:37:32 -0300

    Links active fixes (need refactoring)

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -27,6 +27,7 @@ function Navbar({ offset }) {
         <div className={`logo ${offset > 50 ? 'sm:w-52 w-40' : 'lg:w-auto sm:w-56 w-48'}`}>
           <Link
             to={{ pathname: '/', hash: '#home' }}
+            end
           >
             <img className='' src={logo} alt="Hanover Advocacia Logotipo" />
           </Link>
@@ -43,13 +44,15 @@ function Navbar({ offset }) {
             >
               <NavLink
                 to={'/atuações'}
-                className={`${linkStyle} flex gap-2 items-center`}>
+                className={`${linkStyle} flex gap-2 items-center`}
+                end
+              >
                 <span>Atuações</span>
                 <FaChevronDown className='transition-transform duration-300 group-hover:-rotate-180'></FaChevronDown>
               </NavLink>
               <div
                 id="dropdownHover"
-                className={`z-10 bg-white text-black absolute top-full divide-y divide-gray-100 ease-in-out transition duration-200 rounded-lg shadow w-52 first-letter
+                className={`z-10 dropdown bg-white text-black absolute top-full divide-y divide-gray-100 ease-in-out transition duration-200 rounded-lg shadow w-52 first-letter
                   ${isDropdownVisible ? 'visible opacity-100' : 'opacity-0 invisible'}
                 `}
               >
@@ -57,7 +60,7 @@ function Navbar({ offset }) {
                   {
                     cards.map(actuation => (
                       <li key={actuation.id} >
-                        <NavLink className="block px-4 py-2 bg-white hover:bg-blue-grey-200/70">{actuation.title}</NavLink>
+                        <NavLink to={`/atuações/${actuation.title.toLocaleLowerCase().split(' ').join('-')}`} className="block px-4 py-2 bg-white transition hover:bg-blue-grey-200/70">{actuation.title}</NavLink>
                       </li>
                     ))
                   }
@@ -113,18 +116,26 @@ function Navbar({ offset }) {
               </div>
 
               <div
-                className={`accordion-content z-10 bg-white text-black divide-gray-100 ease-in-out transition-[grid-template-rows] duration-500 rounded-lg shadow w-auto grid overflow-hidden ${isDropdownVisible ? 'grid-rows-1' : 'grid-rows-[0]'}`}
+                className={`dropdown z-10 bg-white text-black divide-gray-100 ease-in-out transition-[grid-template-rows] duration-500 rounded-lg shadow w-auto grid overflow-hidden ${isDropdownVisible ? 'grid-rows-1' : 'grid-rows-[0]'}`}
                 aria-hidden={!isDropdownVisible}
                 aria-labelledby='accordion-trigger'
               >
                 <ul className="py-2 text-sm text-blue-grey-700" aria-labelledby="dropdownHoverButton">
                   <li>
-                    <NavLink to='/atuações' className="block px-4 py-2 bg-white hover:bg-blue-grey-200/70">Todas</NavLink>
+                    <NavLink
+                      to='/atuações'
+                      end
+                      className={`block px-4 py-2 bg-white hover:bg-blue-grey-200/70`}>Todas</NavLink>
                   </li>
                   {
                     cards.map(actuation => (
                       <li key={actuation.id} >
-                        <NavLink className="block px-4 py-2 bg-white hover:bg-blue-grey-200/70">{actuation.title}</NavLink>
+                        <NavLink
+                          to={`/atuações/${actuation.title.toLocaleLowerCase().split(' ').join('-')}`}
+                          className="block px-4 py-2 bg-white hover:bg-blue-grey-200/70"
+                        >
+                          {actuation.title}
+                        </NavLink>
                       </li>
                     ))
                   }
@@ -155,7 +166,7 @@ function Navbar({ offset }) {
         <div className="menu-btn md:hidden block text-3xl cursor-pointer">
           <FaBars onClick={() => setIsMenuVisible(true)}></FaBars>
         </div>
-      </div>
+      </div >
     </div >
   )
 }
--- src/Pages/Actuation.jsx
+++ src/Pages/Actuation.jsx
@@ -6,55 +6,11 @@ import banner from '../assets/banner-bg-empresa.png'
 function Actuation() {
     const { actuation } = useParams()
 
-    const content = [
-        {
-            id: 1,
-            title: "Direito Empresarial",
-            description: "Direito Empresarial e Societário é o ramo do direito relacionado ao estudo das sociedades empresárias (dentre as quais se destacam a sociedade por quotas de responsabilidade limitada e a sociedade anônima, entre outras), bem como das questões que dizem respeito aos sócios e acionistas dessas pessoas jurídicas e às diversas situações que possam ocorrer no seu seio, como alterações de controle e de participação, questões gerenciais, conflitos societários e outros fenômenos.",
-            services: [
-                'Elaboração, revisão e alteração de contratos civis e comerciais;',
-                'Estatuto, Contrato Social, Acordo de Acionista e Joint Venture;',
-                'Constituição e planejamento de estruturas societárias no Brasil e no exterior;',
-                'Representação no Brasil e no exterior de sociedades e/ou seus sócios e acionistas;',
-                'Assessoria jurídica na prevenção de responsabilidade da sociedade e administradores;',
-                'Restruturação societária;',
-                'Due diligence, Governança Corporativa e Compliance;',
-                'Assessoria jurídica para elaboração de Certificate of Incumbency;'
-            ],
-            p1: 'Também chamado de Direito comercial ou direito empresarial, é um ramo do direito privado que pode ser entendido como o conjunto de normas disciplinadoras da atividade negocial do empresário, e de qualquer pessoa física ou jurídica, destinada a fins de natureza econômica, desde que habitual e dirigida à produção de bens ou serviços conducentes a resultados patrimoniais ou lucrativos, e que a exerça com a racionalidade própria de “empresa”, sendo um ramo especial de direito privado. Em Portugal, veja-se direito comercial português. Rege os atos tidos como comerciais. ',
-            p2: 'Assim entendido, o direito empresarial abrange um conjunto variado de matérias, incluindo as obrigações dos empresários, as sociedades empresárias, os contratos especiais de comércio, os títulos de crédito, a propriedade intelectual, entre outras. Desde 1886 nos Estados Unidos, as corporações passaram a ter direitos humanos, antes reservados apenas a pessoas físicas.'
-
-        },
-        {
-            id: 2,
-            title: "Direito teste",
-            description: "Direito Empresarial e Societário é o ramo do direito relacionado ao estudo das sociedades empresárias (dentre as quais se destacam a sociedade por quotas de responsabilidade limitada e a sociedade anônima, entre outras), bem como das questões que dizem respeito aos sócios e acionistas dessas pessoas jurídicas e às diversas situações que possam ocorrer no seu seio, como alterações de controle e de participação, questões gerenciais, conflitos societários e outros fenômenos.",
-            services: [
-                'Elaboração, revisão e alteração de contratos civis e comerciais;',
-                'Estatuto, Contrato Social, Acordo de Acionista e Joint Venture;',
-                'Constituição e planejamento de estruturas societárias no Brasil e no exterior;',
-                'Representação no Brasil e no exterior de sociedades e/ou seus sócios e acionistas;',
-                'Assessoria jurídica na prevenção de responsabilidade da sociedade e administradores;',
-                'Restruturação societária;',
-                'Due diligence, Governança Corporativa e Compliance;',
-                'Assessoria jurídica para elaboração de Certificate of Incumbency;'
-            ],
-            p1: 'Também chamado de Direito comercial ou direito empresarial, é um ramo do direito privado que pode ser entendido como o conjunto de normas disciplinadoras da atividade negocial do empresário, e de qualquer pessoa física ou jurídica, destinada a fins de natureza econômica, desde que habitual e dirigida à produção de bens ou serviços conducentes a resultados patrimoniais ou lucrativos, e que a exerça com a racionalidade própria de “empresa”, sendo um ramo especial de direito privado. Em Portugal, veja-se direito comercial português. Rege os atos tidos como comerciais. ',
-            p2: 'Assim entendido, o direito empresarial abrange um conjunto variado de matérias, incluindo as obrigações dos empresários, as sociedades empresárias, os contratos especiais de comércio, os títulos de crédito, a propriedade intelectual, entre outras. Desde 1886 nos Estados Unidos, as corporações passaram a ter direitos humanos, antes reservados apenas a pessoas físicas.'
-
-        }
-    ]
-
     return (
         <Banner bg={banner}>
             <div>
                 {
-                    content.filter(item => item.title.toLowerCase() === actuation.toLocaleLowerCase().split('-').join(' '))
-                        .map(actuation => (
-                            <div>
-                                <h2 className="title">{actuation.title}</h2>
-                            </div>
-                        ))
+                    actuation
                 }
             </div>
         </Banner>
