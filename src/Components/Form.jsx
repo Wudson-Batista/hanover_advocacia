77a2e397710ac5e4341185a99be1708c98f571c7
Wudson Batista <wudson06b.roberto@gmail.com>
2023-07-31 17:11:18 -0300

    Form with error messages

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,13 +4,19 @@ import { FaPaperPlane } from 'react-icons/fa'
 import { FormProvider, useForm } from 'react-hook-form'
 import { Title } from '../Components/Title/Index'
 import { Input } from '../Components/Input/Index'
+import { yupResolver } from '@hookform/resolvers/yup'
+import validationSchema from '../utils/validation/schema'
+import ErrorMessage from './Input/ErrorMessage'
 
 function Form() {
 
-    const form = useForm();
+    const form = useForm(
+        { resolver: yupResolver(validationSchema), }
+    );
 
     const {
         handleSubmit,
+        formState: { errors }
     } = form
 
     const onSubmit = (data) => {
@@ -30,31 +36,43 @@ function Form() {
                 </div>
                 <div className='mt-8 flex flex-col gap-4'>
                     {/* <FormInput label='Nome' isRequired={true} inputType='text'></FormInput> */}
-                    <Input.Wrapper>
-                        <Input.FormInput field="name" />
-                        <Input.Label htmlFor='floating_name'>
-                            Nome
-                        </Input.Label>
-                    </Input.Wrapper>
-                    <Input.Wrapper>
-                        <Input.FormInput field="email" />
-                        <Input.Label htmlFor='floating_email'>
-                            E-mail
-                        </Input.Label>
-                    </Input.Wrapper>
-                    <Input.Wrapper>
-                        <Input.FormInput field="phone" />
-                        <Input.Label htmlFor='floating_phone'>
-                            Telefone
-                        </Input.Label>
-                    </Input.Wrapper>
+                    <div>
+                        <Input.Wrapper>
+                            <Input.FormInput field="name" />
+                            <Input.Label htmlFor='floating_name'>
+                                Nome
+                            </Input.Label>
+                        </Input.Wrapper>
+                        <ErrorMessage field="name"></ErrorMessage>
+                    </div>
+                    <div>
+                        <Input.Wrapper>
+                            <Input.FormInput field="email" />
+                            <Input.Label htmlFor='floating_email'>
+                                E-mail
+                            </Input.Label>
+                        </Input.Wrapper>
+                        <ErrorMessage field="email"></ErrorMessage>
+                    </div>
+                    <div>
+                        <Input.Wrapper>
+                            <Input.FormInput field="phone" />
+                            <Input.Label htmlFor='floating_phone'>
+                                Telefone
+                            </Input.Label>
+                        </Input.Wrapper>
+                        <ErrorMessage field="phone"></ErrorMessage>
+                    </div>
                     {/* <FormInput label='Mensagem' field='mensage' isRequired={true} inputType='textarea'></FormInput> */}
-                    <Input.Wrapper>
-                        <Input.TextArea field='message' />
-                        <Input.Label htmlFor='floating_message' className='peer-placeholder-shown:top-1/4'>
-                            Mensagem
-                        </Input.Label>
-                    </Input.Wrapper>
+                    <div>
+                        <Input.Wrapper>
+                            <Input.TextArea field='message' />
+                            <Input.Label htmlFor='floating_message' className='peer-placeholder-shown:top-1/4'>
+                                Mensagem
+                            </Input.Label>
+                        </Input.Wrapper>
+                        <ErrorMessage field="message"></ErrorMessage>
+                    </div>
                 </div>
                 <div className='mt-6'>
                     <Button icon={<FaPaperPlane />} type='submit'>
